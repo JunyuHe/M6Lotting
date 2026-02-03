53d561c25a51eb160f75763a91f937458dd130ff
JunyuHe <juhe2@s.sfusd.edu>
2026-02-03 14:08:40 -0800

    M6L5

--- lessons/chest/cheat.gd
+++ lessons/chest/cheat.gd
@@ -1,7 +1,20 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func open() -> void:
+	animation_player.play("open")
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool =(
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
 
 func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
